cc181973bbc3fd75ba1da3ad91fc970a534d1ad7
Sonu Giri <sonugiri1043@gmail.com>
2019-06-02 12:43:29 +0530

    unique_ptr example usage

--- Unique_ptr/Unique_ptr.cpp
+++ Unique_ptr/Unique_ptr.cpp
@@ -6,6 +6,23 @@ using std::endl;
 using std::unique_ptr;
 
 // g++ Unique_ptr.cpp --std=c++14
+// make_unique is only supported after c++14
+
+/*
+  In unique_ptr<>, one and only one unique_ptr<> has the Ownership
+  of the Object and manages its lifetime. Its possible to transfer
+  the ownership using std::move(). However, once std::move() is called
+  then moved pointer will no longer hold the ownership.
+
+  Just like shared_ptr<>, unique_ptr<> can also be returned from a Function.
+  Why isn't transfer of ownership is prohibited in unique_ptr<> ?
+  Compilers explicit add std::move() while returning the unique_ptr<>
+  from a function which transfers the ownership.
+
+  The C++14 specification has added the support of make_unique<>().
+  Its usage is similar to that of make_shared<>() and should be used instead of new.
+
+ */
 
 class Test {
 public:
@@ -52,11 +69,28 @@ unique_ptr<Test> fn_return_unique_ptr() {
   //Compiler changes this to "return move( uni )
   return uni;
 }
+
+void Unique_ptr_creation_using_make_unique() {
+  unique_ptr<Test> ptr = std::make_unique<Test>();
+}
+
+void make_unique_without_ownership_trasnfer() {
+  std::make_unique<Test>();
+}
  
+void make_unique_with_array() {
+  unique_ptr< Test[] > ptr= std::make_unique<Test[]>(10);
+  ptr[0].show();
+  ptr[1].show();
+}
+
 int main() {
   Test_unique_ptr_ReleaseMemory_when_out_of_scope();
   Test_unique_ptr_No_OwnerShip_Transfer();
   Test_ownership_Transfer_using_move();
   fn_return_unique_ptr();
+  Unique_ptr_creation_using_make_unique();
+  make_unique_without_ownership_trasnfer();
+  make_unique_with_array();
   return 0;
 }
